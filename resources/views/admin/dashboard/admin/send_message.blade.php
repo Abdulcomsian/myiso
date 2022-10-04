ec66be1e3e9d32777b00a117adfc1f33641b59f5
Abdul Basit <basitawan.abdul@gmail.com>
2022-10-04 10:43:57 +0500

    updated code

--- resources/views/admin/dashboard/admin/send_message.blade.php
+++ resources/views/admin/dashboard/admin/send_message.blade.php
@@ -208,7 +208,7 @@
 			var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 			var yyyy = today.getFullYear();
 
-			today  = dd + '-' + mm + '-' + yyyy;
+			today  = mm + '-' + dd + '-' + yyyy;
 			$('input[name="daterange"]').daterangepicker({
 				showDropdowns: true,
 				changeYear: true,
