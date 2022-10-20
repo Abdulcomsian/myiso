4198bffd4eae3e57637348a359048062303b0288
Abdul Basit <basitawan.abdul@gmail.com>
2022-10-20 15:49:54 +0500

    updated code

--- resources/views/dashboard/explainer_videos.blade.php
+++ resources/views/dashboard/explainer_videos.blade.php
@@ -18,7 +18,7 @@
 	
 	@foreach($videos as $video)
 	<!--Video--->
-	<div class="col-md-4 mt-2 mb-2">
+	<div class="col-md-4 mt-2 mb-2" style="    text-align: center;">
 	<a href="{{url('public/uploads/explainer_videos/'.$video->video)}}" target="_blank">
 		<img src="{{ asset('assets/media/icons')}}/mp4.png" class="img-thumbnail img-fluid"></a>
 	
