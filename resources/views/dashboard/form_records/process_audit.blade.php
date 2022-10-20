97f504be96501cbb7b9554afc5c6c51156095e1c
Abdul Basit <basitawan.abdul@gmail.com>
2022-10-20 13:19:52 +0500

    updated code

--- resources/views/dashboard/form_records/process_audit.blade.php
+++ resources/views/dashboard/form_records/process_audit.blade.php
@@ -879,7 +879,7 @@
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="form-group">
-                                    <label>Attach Evidence:</label>
+                                    <label>Attach Evidence <span class="text-danger" style="color:#000 !important;">(jpeg, mp3, mp4, .xls, doc)</span>:</label>
                                     <input name="attach_evidence" type="file" class="form-control"
                                            accept="image/*,.doc, .docx,.txt,.pdf">
                                 </div>
@@ -1260,7 +1260,7 @@
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="form-group">
-                                    <label>Attach Evidence:</label>
+                                    <label>Attach Evidence <span class="text-danger" style="color:#000 !important;">(jpeg, mp3, mp4, .xls, doc)</span>:</label>
                                     <div class="evidence_attachemnt_div"></div>
                                 </div>
                             </div>
