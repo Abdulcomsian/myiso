1aa41d4ecb7d78a7f662fe1a1cdf727d41a93a1b
Abdul Basit <89443860+Haroonniaz96github@users.noreply.github.com>
2022-11-02 17:02:46 +0500

    new changes

--- resources/views/dashboard/form_records/chemical_control.blade.php
+++ resources/views/dashboard/form_records/chemical_control.blade.php
@@ -505,8 +505,8 @@
 
     <div class="modal fade" id="viewinterestedparty" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
          aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content ">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content modal-lg">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">View Chemical Control Details</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
