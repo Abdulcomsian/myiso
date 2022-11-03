c8f7d654114a38b181a20719abb94c32c8bb90f4
Abdul Basit <basitawan.abdul@gmail.com>
2022-11-03 15:48:28 +0500

    updated code

--- resources/views/admin/adminform_records/employess.blade.php
+++ resources/views/admin/adminform_records/employess.blade.php
@@ -246,7 +246,7 @@
 													<!-- href="{{ asset($item->cv) }}" -->
 													<!-- Modal -->
 												<div class="modal fade" id="cv{{$item->id}}" tabindex="-1" role="dialog" aria-labelledby="model1Label" aria-hidden="true">
-													<div class="modal-dialog" role="document">
+													<div class="modal-dialog" style="width:75%;" role="document">
 													<div class="modal-content">
 													<div class="modal-header">
 													<a href="{{ asset($item->cv) }}" download>
