0f3aef47ac51aa4fad807d53ccf2469fd27ce0d3
MuneebAccrual <muneeb.rehman@accrualhub.com>
2023-12-18 15:17:30 +0500

    inline css added for outlook

--- resources/views/mails/sixMonthEmail.blade.php
+++ resources/views/mails/sixMonthEmail.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Subject</title>
     <style>
+        /* Inline styles for body */
         body {
             font-family: 'Arial', sans-serif;
             background-color: #f4f4f4;
@@ -14,72 +15,17 @@
             margin: 0;
             padding: 0;
         }
-
-        .container {
-            max-width: 500px;
-            height: auto;
-            margin: 50px auto;
-            /* Center the container vertically */
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-
-        /* p {
-            margin: 20px 0;
-            font-size: 16px;
-            line-height: 1.6;
-            text-align: center;
-            
-        } */
-
-        .content-area {
-            text-align: left;
-        }
-
-        img {
-            /* max-width: 100%; */
-            height: 50px;
-            margin-bottom: 20px;
-        }
-
-        /* Basic button styles */
-        .button {
-            display: inline-block;
-            padding: 10px 20px;
-            font-size: 16px;
-            font-weight: bold;
-            text-align: center;
-            text-decoration: none;
-            cursor: pointer;
-            border: 2px solid #3498db;
-            color: #fff;
-            background-color: #3498db;
-            border-radius: 5px;
-            transition: background-color 0.3s, color 0.3s, border-color 0.3s;
-        }
-
-        /* Hover effect */
-        .button:hover {
-            background-color: #2980b9;
-            border-color: #2980b9;
-        }
-
-
-        footer {
-            margin-top: 20px;
-            text-align: center;
-            color: #888;
-        }
     </style>
 </head>
 
-<body>
-    <div class="container">
-        <img src="https://myisoonline.com/assets/media/logos/MyISOOnline-Logo.png" alt="Logo">
-        <div class="content-area">
+<body style="font-family: 'Arial', sans-serif; background-color: #f4f4f4; color: #333; margin: 0; padding: 0;">
+
+    <div class="container" style="max-width: 500px; height: auto; margin: 50px auto; padding: 20px; background-color: #fff; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); text-align: center;">
+
+        <img src="https://myisoonline.com/assets/media/logos/MyISOOnline-Logo.png" alt="Logo" style="height: 50px; margin-bottom: 20px;">
+
+        <div class="content-area" style="text-align: left;">
+
             <p><strong>Dear Sir’s</strong></p>
             <p>
                 Your ISO Certificate's validity depends on maintaining your documentation. As of now, insufficient activity has been made on your account. Please act to update and maintain your records
@@ -88,13 +34,18 @@
                 For any assistance or clarification, please visit the support section on your account where you will find video tutorials and other useful tools. Alternatively, our team is ready to support you with any questions you may have.
             </p>
             <p>Your prompt attention is crucial to ensure the continuous validity of your ISO certifications.</p>
+
         </div>
 
-        <a class="button" href="https://myisoonline.com/" target="_blank">Sign In</a>
+        <a href="https://myisoonline.com/" target="_blank" style="display: inline-block; padding: 10px 20px; font-size: 16px; font-weight: bold; text-align: center; text-decoration: none; cursor: pointer; border: 2px solid #3498db; color: #fff; background-color: #3498db; border-radius: 5px; transition: background-color 0.3s, color 0.3s, border-color 0.3s;"
+            class="button">Sign In</a>
+
     </div>
-    <footer>
-        <p style="text-align: center;">All Rights Reserved. MyISOOnline</p>
+
+    <footer style="margin-top: 20px; text-align: center; color: #888;">
+        <p>All Rights Reserved. MyISOOnline</p>
     </footer>
+
 </body>
 
 </html>
--- resources/views/mails/tenMonthEmail.blade.php
+++ resources/views/mails/tenMonthEmail.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Subject</title>
     <style>
+        /* Inline styles for body */
         body {
             font-family: 'Arial', sans-serif;
             background-color: #f4f4f4;
@@ -14,72 +15,17 @@
             margin: 0;
             padding: 0;
         }
-
-        .container {
-            max-width: 500px;
-            height: auto;
-            margin: 50px auto;
-            /* Center the container vertically */
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-
-        /* p {
-            margin: 20px 0;
-            font-size: 16px;
-            line-height: 1.6;
-            text-align: center;
-            
-        } */
-
-        .content-area {
-            text-align: left;
-        }
-
-        img {
-            /* max-width: 100%; */
-            height: 50px;
-            margin-bottom: 20px;
-        }
-
-        /* Basic button styles */
-        .button {
-            display: inline-block;
-            padding: 10px 20px;
-            font-size: 16px;
-            font-weight: bold;
-            text-align: center;
-            text-decoration: none;
-            cursor: pointer;
-            border: 2px solid #3498db;
-            color: #fff;
-            background-color: #3498db;
-            border-radius: 5px;
-            transition: background-color 0.3s, color 0.3s, border-color 0.3s;
-        }
-
-        /* Hover effect */
-        .button:hover {
-            background-color: #2980b9;
-            border-color: #2980b9;
-        }
-
-
-        footer {
-            margin-top: 20px;
-            text-align: center;
-            color: #888;
-        }
     </style>
 </head>
 
-<body>
-    <div class="container">
-        <img src="https://myisoonline.com/assets/media/logos/MyISOOnline-Logo.png" alt="Logo">
-        <div class="content-area">
+<body style="font-family: 'Arial', sans-serif; background-color: #f4f4f4; color: #333; margin: 0; padding: 0;">
+
+    <div class="container" style="max-width: 500px; height: auto; margin: 50px auto; padding: 20px; background-color: #fff; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); text-align: center;">
+
+        <img src="https://myisoonline.com/assets/media/logos/MyISOOnline-Logo.png" alt="Logo" style="height: 50px; margin-bottom: 20px;">
+
+        <div class="content-area" style="text-align: left;">
+
             <p><strong>Dear Sir’s</strong></p>
             <p>
                 This is a follow-up to our recent communications regarding the importance of updating your documentation, to avoid your certificate being revoked.
@@ -87,13 +33,18 @@
             <p>
                 Time is of the essence. Please act urgently to ensure the continued validity of your ISO certification. 
             </p>
+
         </div>
 
-        <a class="button" href="https://myisoonline.com/" target="_blank">Sign In</a>
+        <a href="https://myisoonline.com/" target="_blank" style="display: inline-block; padding: 10px 20px; font-size: 16px; font-weight: bold; text-align: center; text-decoration: none; cursor: pointer; border: 2px solid #3498db; color: #fff; background-color: #3498db; border-radius: 5px; transition: background-color 0.3s, color 0.3s, border-color 0.3s;"
+            class="button">Sign In</a>
+
     </div>
-    <footer>
-        <p style="text-align: center;">All Rights Reserved. MyISOOnline</p>
+
+    <footer style="margin-top: 20px; text-align: center; color: #888;">
+        <p style="margin: 0;">All Rights Reserved. MyISOOnline</p>
     </footer>
+
 </body>
 
 </html>
