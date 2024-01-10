14e94717bba14414d7249c9fba1479bc5e36f7a9
MuneebAccrual <muneeb.rehman@accrualhub.com>
2024-01-10 21:58:17 +0500

    Myiso Changes

--- resources/views/admin/dashboard/admin/view_user.blade.php
+++ resources/views/admin/dashboard/admin/view_user.blade.php
@@ -480,7 +480,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Activity Reminder Details</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Activity Reminder</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
