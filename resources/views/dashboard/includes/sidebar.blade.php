b881c4e1b37b2773835db8c4ad1c09b6a70ecfc5
msaeedinfomist <saeed.mudassar@gmail.com>
2024-10-07 23:41:57 +0500

    new course sider bar changes

--- resources/views/dashboard/includes/sidebar.blade.php
+++ resources/views/dashboard/includes/sidebar.blade.php
@@ -574,12 +574,12 @@
 						</ul>
 					</div>
 				</li>
-					<li class="kt-menu__item   kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover">
+				<li class="kt-menu__item   kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover">
 					<a href="javascript:;" class="kt-menu__link kt-menu__toggle">
 						<span class="kt-menu__link-icon">
 							<i class="fab fa-wpforms"></i>
 						</span>
-						<span class="kt-menu__link-text">ISO Online Courses</span>
+						<span class="kt-menu__link-text">ISO Certified Courses</span>
 						<i class="kt-menu__ver-arrow la la-angle-right"></i>
 					</a>
 					<div class="kt-menu__submenu ">
@@ -658,6 +658,37 @@
 								</a>
 							</li>  --}}
 
+						</ul>
+					</div>
+				</li>
+				<li class="kt-menu__item   kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover">
+					<a href="javascript:;" class="kt-menu__link kt-menu__toggle">
+						<span class="kt-menu__link-icon">
+							<i class="fab fa-wpforms"></i>
+						</span>
+						<span class="kt-menu__link-text">Courses</span>
+						<i class="kt-menu__ver-arrow la la-angle-right"></i>
+					</a>
+					<div class="kt-menu__submenu ">
+						<span class="kt-menu__arrow"></span>
+						<ul class="kt-menu__subnav">
+							<li class="kt-menu__item " aria-haspopup="true">
+								<a href="https://myisoonline.com/public/lms/courses/water-conservation-and-reducing-water-waste-in-schools/" class="kt-menu__link " target="_blank">
+									<i class="kt-menu__link-bullet kt-menu__link-bullet--dot">
+										<span></span>
+									</i>
+									<span class="kt-menu__link-text"> Water Conservation and Reducing Water Waste in Schools</span></a>
+							</li>
+                              <br>
+							<li class="kt-menu__item " aria-haspopup="true">
+								<a href="https://myisoonline.com/public/lms/courses/maintaining-equipment-and-tools-in-small-construction-sites-and-office-locations/" class="kt-menu__link" target="_blank">
+									<i class="kt-menu__link-bullet kt-menu__link-bullet--dot">
+										<span></span>
+									</i>
+									<span class="kt-menu__link-text"> Maintaining Equipment and Tools in Small Construction Sites and Office Locations</span>
+								</a>
+							</li>
+							
 						</ul>
 					</div>
 				</li>
