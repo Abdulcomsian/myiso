29d995a3b42442b71c4f6ecc1fe0180af5cecd19
Abdul Basit <basitawan.abdul@gmail.com>
2026-06-15 17:45:47 +0500

    laavel version update

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -1,14 +1,11 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
 class OrderSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
         //
