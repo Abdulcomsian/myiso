de3c0ef56b4f0055a6a612364adfe9bacaec202d
Abdul Basit <basitawan.abdul@gmail.com>
2026-06-16 14:04:19 +0500

    laravel version update working

--- config/app.php
+++ config/app.php
@@ -173,11 +173,6 @@ return [
         /*
          * Application Service Providers...
          */
-        // App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
         
     ],
